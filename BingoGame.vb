45b04be531ac7c7804aabca506b784e8c8a5b015
Owen Fujii <dalekrebel@gmail.com>
2024-02-05 08:34:19 -0700

    Add project files.

---
+++ BingoGame.vb
@@ -0,0 +1,23 @@
+﻿'Bingo Game
+'Spring 2024
+
+Option Strict On
+Option Explicit On
+
+'TODO
+'[] Start New Game
+'[] Draw One Ball
+'[] Track Drawn Ball
+'[] Display Ball #
+'[] Draw Two Ball
+'[] Repeat but no repeat balls can be displayed
+Module BingoGame
+
+    Sub Main()
+        Game2.Display()
+
+
+        Console.Read()
+    End Sub
+
+End Module
---
+++ Game2.vb
@@ -0,0 +1,27 @@
+﻿Module Game2
+    Sub NewGame()
+
+    End Sub
+
+    Sub DrawBall()
+
+    End Sub
+
+    Sub Display()
+
+        Dim temp(14, 4) As Boolean
+
+        temp(7, 3) = True
+        temp(3, 4) = True
+        For row = 0 To 14
+            For column = 0 To 4
+                If temp(row, column) Then
+                    Console.Write($"X C")
+                Else
+                    Console.Write("/")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+End Module
