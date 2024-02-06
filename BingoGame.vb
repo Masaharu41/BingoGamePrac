30717af3df6e5f12358bd442b3fd0d310ac9ae37
Owen Fujii <dalekrebel@gmail.com>
2024-02-06 08:55:44 -0700

    Added New Game and Tracker

--- BingoGame.vb
+++ BingoGame.vb
@@ -1,30 +1,50 @@
-﻿'Bingo Game
-'Spring 2024
-
-Option Strict On
+﻿Option Strict On
 Option Explicit On
 Option Compare Text
+'Bingo Game
+'Spring 2024
+
+Imports System.Net.Http
+
+
 
 'TODO
-'[] Start New Game
-'[] Draw One Ball
-'[] Track Drawn Ball
-'[] Display Ball #
-'[] Draw Two Ball
-'[] Repeat but no repeat balls can be displayed
+'[*] Start New Game
+'[*] Draw One Ball
+'[*] Track Drawn Ball
+'[*] Display Ball #
+'[*] Draw Two Ball
+'[*] Repeat but no repeat balls can be displayed
 Module BingoGame
 
     Sub Main()
         Dim userInput As String
         Dim tracker(14, 4) As Boolean ' each letter had 15 unique #
-
+        Dim ballsDrawn As Integer = 0
+        Dim message As String = "Hello There!"
+        Game2.NewGame(tracker)
         Do
-            Game2.NewGame()
             Console.Clear()
-            Game2.Display()
-            Console.WriteLine("Prompt")
+            Game2.Display(tracker)
+            Console.WriteLine(message)
             userInput = Console.ReadLine()
+            If userInput = "n" Then
+                Game2.NewGame(tracker)
+            Else
+                If ballsDrawn >= 75 Then
+                    message = "All balls have been drawn"
+                    Exit Do
+
+                Else
+                    Game2.DrawBall(tracker)
+                    ballsDrawn += 1
+                    message = "Press enter to draw, N for new game, or Q for quit"
+                End If
+            End If
+
+
         Loop Until userInput = "q"
+        Console.WriteLine("Have a Nice Day!")
         Console.Read()
     End Sub
 
